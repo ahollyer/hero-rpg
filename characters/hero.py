54a47f9ca80c24542df2b83c95aa22845b9e6770
Aspen Hollyer <aspen.dollins@gmail.com>
2017-04-26 11:56:10 -0500

    Add buy function to Hero

--- characters/hero.py
+++ characters/hero.py
@@ -3,7 +3,15 @@ import characters.base
 class Hero (Character):
     def __init__ (self, name='Samwise Gamgee', health=10, power=5):
         super().__init__(name, health, power)
+
     @classmethod
     def create(cls):
         name = input('What\'s your Hero\'s name?: ')
         return cls(name)
+
+    def buy(self, item):
+        if self.coins > item.cost:
+            self.coins -= item.cost
+            item.apply(self)
+        else:
+            print("You can't afford this!")
