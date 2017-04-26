bdfbdbc5fbb7ad018089c64ae04bccb322212d39
meowmeowcomputers <ryansimonleon@gmail.com>
2017-04-26 15:55:25 -0500

    Added Character requirements 1-6

--- characters.py
+++ characters.py
@@ -1,19 +1,24 @@
 from random import randint
 
 class Character:
-    def __init__(self, name, health, power):
+    def __init__(self, name, health, power, evade, coin):
         self.health = health
         self.power = power
         self.name = name
-    # def attack(self, target): #Propose removing attack function from main Character class and moving to individual characters
-    #     target.health -= self.power
-    #     print("{} does {} damage to {}.".format(
-    #         self.name, self.power, target.name))
-    #     if target.health <= 0:
-    #         print("{} is dead.".format(target.name))
+        self.evade = evade
+        self.max_health = health
+    def attack(self, target):
+        self.special(target) #Specials before attacks
+        target.health -= self.power
+        print("{} does {} damage to {}.".format(self.name, self.power,
+        target.name))
+        # if target.health <= 0:
+        #     print("{} is dead.".format(target.name))
+    def special(self, target):
+        pass
     def item(self, health, power): #Item use function
         print('Item Placeholder')
-    def heal(self, healHP):
+    def heal(self, healHP): #Healing function
         self.health += healHP
         print('{} heals {} damage!'.format(self.name, healHP))
     def action(self, target):#Placeholder for flexibility to define a Character's actions beyond attack and Do Nothing
@@ -27,58 +32,65 @@ class Character:
         print("{} has {} health and {} power.".format(self.name, self.health, self.power))
 
 class Hero (Character):
-    def __init__ (self, name, health=100, power=5):
-        super().__init__(name, health, power)
+    def __init__ (self, name, health, power, evade, coin):
+        super().__init__(name, health, power, evade, coin)
     def result(self, target):
         print('Resultplaceholder')
     @classmethod
     def create(cls):
-        #name = input('What\'s your Hero\'s name?: ')
-        heroClass = int(input('What character would you like to play? \
-            \n1. Steiner the Fighter (Chance to Critical Hit for double damage)\
-            \n2. Garnet the Medic (Chance to heal self when she attacks)'))
-        return cls(heroClass)
+        name = input('What\'s your Hero\'s name?: ')
+        return cls(name)
 
 class Enemy (Character):
-    def __init__ (self, name = 'Shagrat the goblin', health=6, power=2):
-        super().__init__(name, health, power)
-    def attack(self, target):
-        target.health -= self.power
-        print("{} does {} damage to {}.".format(
-            self.name, self.power, target.name))
-        if target.health <= 0:
-            print("{} is dead.".format(target.name))
+    def __init__ (self, name, health, power, evade, coin):
+        super().__init__(name, health, power, evade, coin)
+    def checkLife(self, target):
+        if self.alive():
+            if target.evade > 2:
+                print('{} evaded the attack from {}!'.format(target.name, self.name))
+            else:
+                self.attack(target)
+        else:
+            print("{} is dead.".format(self.name))
+
+class Zombie (Enemy):
+    def __init__ (self, name, health, power, evade, coin):
+        super().__init__(name, health, power, evade, coin)
+    def alive(self):
+        if self.health < 0:
+            print('Z O M B I E S  D O  N O T  D I E')
+        return True
 
 class Fighter (Hero):
-    def __init__ (self, name = 'Steiner the Fighter', health=200, power=10):
-        super().__init__(name, health, power)
-    def attack(self, target):
-        crit = randint(1,5)
-        if crit == 5:
-            target.health -= self.power*2
-            print("BOOM! Critical hit! {} does {} damage to {}.".format(
-                self.name, self.power*2, target.name))
-        else:
-            target.health -= self.power
-            print("{} does {} damage to {}.".format(
-                self.name, self.power, target.name))
+    def __init__ (self, name, health=200, power=10, evade=0, coin=0):
+        super().__init__(name, health, power, evade, coin)
+    def special(self, target):
+        crit(self, target)
 
 class Medic (Hero):
-    def __init__ (self, name = 'Garnet the Medic', health=600, power=3, spellpower=2):
-        super().__init__(name, health, power)
-    def attack(self, target):
-        spell = randint(1,5)
-        healHP = randint(self.spellpower, self.spellpower+3)
-        if spell == 5:
-            self.heal(healHP)
-            target.health -= self.power
-            print("{} does {} damage to {}!".format(
-                self.name, self.power, target.name))
-        else:
-            target.health -= self.power
-            print("{} does {} damage to {}.".format(
-                self.name, self.power, target.name))
+    def __init__ (self, name, health=600, power=3, evade=0, coin=0):
+        super().__init__(name, health, power, evade, coin)
+    def special(self, target):
+        healAttack(self, target)
 
-class Ninja (Character):
-    def __init__ (self, name = 'Shadow the Ninja', health=6, power=3):
-        super().__init__(name, health, power)
+class Ninja (Hero):
+    def __init__ (self, name, health=600, power=3, evade=0, coin=0):
+        super().__init__(name, health, power, evade, coin)
+    def special(self, target):
+        dodge(self)
+
+def dodge(self):
+    self.evade = randint(1,10)
+
+def crit(self, target):
+    critChance = randint(1,5)
+    if critChance == 5:
+        target.health -= self.power
+        print("BOOM! Critical hit! {} does {} additional damage to {}.".format(
+            self.name, self.power, target.name))
+
+def healAttack(self, target):
+    spell = randint(1,5)
+    healHP = randint(self.power, self.power+3)
+    if spell == 5:
+        self.heal(healHP)
--- hero-rpg.py
+++ hero-rpg.py
@@ -8,20 +8,32 @@ In this simple RPG game, the hero fights the goblin. He has the options to:
 3. flee
 
 """
-from characters import Character, Hero, Enemy, Medic
+from characters import Character, Hero, Enemy, Medic, Fighter, Ninja, Zombie
 
 def main():
-    hero = Hero.create()
+    heroClass = input('What character would you like to play? \
+        \n1. Fighter (Chance to Critical Hit for double damage)\
+        \n2. Medic (Chance to heal self when she attacks)\
+        \n3. Ninja (High chance to evade attacks, but very low health)\
+        \n')
+    if heroClass == "1":
+        hero = Fighter.create()
+    elif heroClass == "2":
+        hero = Medic.create()
+    elif heroClass == "3":
+        hero = Ninja.create()
+    else:
+        print("Invalid input {}".format(inpt))
 
     waiting = True
     while waiting:
         ans = input('You are stumbling through a dark forest. To your left is a zombie-ridden path, and to your right is a cave full of goblins. What do you do?\n1. Turn left\n2. Turn right\n')
         if ans == '1':
-            enemy = Enemy('ZombieBob', 600, 4)
+            enemy = Zombie('ZombieBob', 10, 4, 0, 0)
             print("You quietly creep down the path, but a zombie ambushes you! You must fight!")
             waiting = False
         elif ans == '2':
-            enemy = Enemy()
+            enemy = Enemy('Shagrat the Goblin', 10, 3, 0, 0)
             print("The cave is damp and smells of goblin urine. You hear a noise behind you, and turn just in time to duck away from a goblin's blow. You must fight!")
             waiting = False
         else:
@@ -46,10 +58,8 @@ def main():
             break
         else:
             print("Invalid input {}".format(inpt))
-
-        if enemy.alive():
-            # Goblin attacks hero
-            enemy.attack(hero)
+        enemy.checkLife(hero)
+        #The enemy.checkLife function  has replace if Enemy alive loop. The enemy attacks through actions within the checkLife function
 
 if __name__ == "__main__":
   main()
