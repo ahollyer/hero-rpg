326452b46fe6ec151708523b7c3f9b1674048532
Aspen Hollyer <aspen.dollins@gmail.com>
2017-04-26 11:58:44 -0500

    Create store

---
+++ envs/store.py
@@ -0,0 +1,23 @@
+class Store(object):
+    # If you define a variable in the scope of a class:
+    # This is a class variable and you can access it like
+    # Store.items => [Tonic, Sword]
+    items = [Tonic, Sword]
+    def do_shopping(self, hero):
+        while True:
+            print("=====================")
+            print("Welcome to the store!")
+            print("=====================")
+            print("You have {} coins.".format(hero.coins))
+            print("What do you want to do?")
+            for i in range(len(Store.items)):
+                item = Store.items[i]
+                print("{}. buy {} ({})".format(i + 1, item.name, item.cost))
+            print("10. leave")
+            input = int(input("> "))
+            if input == 10:
+                break
+            else:
+                ItemToBuy = Store.items[input - 1]
+                item = ItemToBuy()
+                hero.buy(item)
