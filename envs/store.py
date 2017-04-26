bb7c375ab1ad3aef955074158fb49d3cb7e73d84
Aspen Hollyer <aspen.dollins@gmail.com>
2017-04-26 14:15:51 -0500

    Create basic shop method

--- envs/store.py
+++ envs/store.py
@@ -1,23 +1,31 @@
-class Store(object):
+import time
+
+from items.armor import Armor
+# from items.tonic import Tonic
+# from items.weapon import Weapon
+
+class Store:
     # If you define a variable in the scope of a class:
     # This is a class variable and you can access it like
     # Store.items => [Tonic, Sword]
-    items = [Tonic, Sword]
-    def do_shopping(self, hero):
+    shirt = Armor("A Ragged Tunic", 5, "This tunic has seen better days. It will provide some protection (+2 health).\n")
+    items = [shirt]
+    def shop(self, hero):
+        print("=====================")
+        print("Welcome to the store!")
+        print("=====================")
         while True:
-            print("=====================")
-            print("Welcome to the store!")
-            print("=====================")
             print("You have {} coins.".format(hero.coins))
-            print("What do you want to do?")
+            print("What do you want to do?\n")
             for i in range(len(Store.items)):
                 item = Store.items[i]
-                print("{}. buy {} ({})".format(i + 1, item.name, item.cost))
+                print("{}. Buy {} ({} coins) - {}".format(i + 1, item.name, item.cost, item.desc))
             print("10. leave")
-            input = int(input("> "))
-            if input == 10:
+            choice = int(input("> "))
+            if choice == 10:
                 break
             else:
-                ItemToBuy = Store.items[input - 1]
-                item = ItemToBuy()
+                item = Store.items[choice - 1]
                 hero.buy(item)
+                time.sleep(2)
+                print("Thanks for your business.\n")
